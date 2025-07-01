103c1fae4ce40b710332ca2c8e5802438043d841
Jared Magnusson <jmag722@gmail.com>
2025-07-01 08:09:26 -0500

    WIP #14: taylor-maccoll solution given cone angle, iterating to find shock

--- minuteman/compressible/cone_flow.py
+++ minuteman/compressible/cone_flow.py
@@ -28,6 +28,73 @@ def fun(theta, y, gam):
     return dx1prime, dx2prime
 
 
+def taylor_maccoll_from_cone(M1: float, cone_angle: float, gam: float = 1.4):
+    """Compute the solution for a right cone at zero AoA.
+
+    This follows the solution method to the Taylor-Maccoll analysis, as presented
+    in Anderson Ch 10.
+
+    Args:
+        M1 (float): freestream Mach number [-]
+        shock_angle (float): oblique shock angle [radians]
+        gam (float, optional): ratio of specific heats. Defaults to 1.4.
+    """
+    initial_shock_guess = obs.shock_angle(
+        M1=M1, theta=cone_angle, gam=gam)
+    initial_shock_guess = obs.mach_angle(M=M1)
+    initial_shock_guess = np.radians(34)
+
+    print(f"Freestream Mach at gamma: {M1} at gam={gam}")
+
+    solution = _tay_mac_cone(
+        shock_angle_guess=initial_shock_guess,
+        cone_angle=cone_angle,
+        M1=M1,
+        gam=gam)
+    vr, vtheta = solution.y
+    theta = solution.t
+    sol_vprime = (vr**2 + vtheta**2)**0.5
+    sol_mach = ((sol_vprime**-2 - 1) * (gam-1)/2) ** -0.5
+
+    print(f"Mach at surface: {sol_mach[-1]}")
+    print(f"Shock Angle: {np.degrees(theta[0])} deg")
+    print(
+        f"Deflection Angle: {np.degrees(obs.deflection_angle(M1=M1, beta=theta[0], gam=gam))} deg")
+    print(f"Cone angle = {np.degrees(theta[-1])} deg")
+    print(f"Vtheta at cone = {np.degrees(vtheta[-1])}")
+
+
+def _tay_mac_cone(shock_angle_guess: float, M1: float,
+                  cone_angle: float, gam: float,
+                  epsilon: float = 1e-15, ntheta: int = 100):
+    mn1 = obs.mach1_normal(M1=M1, beta=shock_angle_guess)
+    mn2 = obs.mach2_normal(Mn1=mn1, gam=gam)
+    deflection_angle = obs.deflection_angle(
+        M1=M1, beta=shock_angle_guess, gam=gam)
+    m2 = obs.mach2(Mn2=mn2, beta=shock_angle_guess, theta=deflection_angle)
+    v_shock = compute_vprime(m=m2, gam=gam)
+    vtheta_shock = compute_vtheta_shock(vprime=v_shock, shock_angle=shock_angle_guess,
+                                        deflection_angle=deflection_angle)
+    vr_shock = compute_vr_shock(vprime=v_shock, shock_angle=shock_angle_guess,
+                                deflection_angle=deflection_angle)
+
+    theta_arr = np.linspace(shock_angle_guess, cone_angle, ntheta)
+    solution = solve_ivp(fun=fun, t_span=(
+        shock_angle_guess, cone_angle), y0=(vr_shock, vtheta_shock), args=(gam,), t_eval=theta_arr)
+
+    sol_vr, sol_vtheta = solution.y
+    cone_angle_new = (cone_angle - sol_vtheta[-1] /
+                      fun(theta=theta_arr[-1], y=(sol_vr[-1], sol_vtheta[-1]), gam=gam)[-1])
+    # we know the actual cone angle (it's cone_angle), so adjust the new shock
+    # angle by the amount needed to get vtheta(theta_c)=0
+    shock_angle_new = shock_angle_guess - (cone_angle_new - cone_angle)
+    if np.abs(cone_angle_new - cone_angle) < epsilon:
+        return solution
+    else:
+        return _tay_mac_cone(shock_angle_guess=shock_angle_new, M1=M1,
+                             cone_angle=cone_angle, gam=gam)
+
+
 def taylor_maccoll_from_shock(M1: float, shock_angle: float, gam: float = 1.4):
     """Compute the solution for a right cone at zero AoA.
 
@@ -102,10 +169,8 @@ def _tay_mac(cone_angle_guess: float, vr_shock: float, vtheta_shock: float,
     sol_vr, sol_vtheta = solution.y
     vrc = sol_vr[-1]
     vthetac = sol_vtheta[-1]
-    cone_angle_new = (cone_angle_guess - (vthetac - 0) /
+    cone_angle_new = (cone_angle_guess - vthetac /
                       fun(theta=theta_arr[-1], y=(vrc, vthetac), gam=gam)[-1])
-    # print(f"Guessed Cone angle = {np.degrees(theta_arr[-1])} deg")
-    # print(f"Computed Vtheta at cone = {np.degrees(vthetac)}")
     if np.abs(cone_angle_new - cone_angle_guess) < epsilon:
         return solution
     else:
@@ -114,10 +179,18 @@ def _tay_mac(cone_angle_guess: float, vr_shock: float, vtheta_shock: float,
 
 
 if __name__ == "__main__":
-    taylor_maccoll_from_shock(M1=3.0, shock_angle=np.radians(25), gam=1.4)
+    # taylor_maccoll_from_shock(M1=3.0, shock_angle=np.radians(25), gam=1.4)
+    # print("\n")
+    # taylor_maccoll_from_shock(M1=7.0, shock_angle=np.radians(80), gam=1.4)
+    # print("\n")
+    # taylor_maccoll_from_shock(M1=4.0, shock_angle=np.radians(33), gam=1.5)
+    # print("\n")
+    # taylor_maccoll_from_shock(M1=1.5, shock_angle=np.radians(50), gam=1.5)
+
+    taylor_maccoll_from_cone(M1=11.0, cone_angle=np.radians(25), gam=1.6)
     print("\n")
-    taylor_maccoll_from_shock(M1=7.0, shock_angle=np.radians(80), gam=1.4)
-    print("\n")
-    taylor_maccoll_from_shock(M1=4.0, shock_angle=np.radians(33), gam=1.5)
-    print("\n")
-    taylor_maccoll_from_shock(M1=1.5, shock_angle=np.radians(50), gam=1.5)
+    # taylor_maccoll_from_shock(M1=7.0, shock_angle=np.radians(80), gam=1.4)
+    # print("\n")
+    # taylor_maccoll_from_shock(M1=4.0, shock_angle=np.radians(33), gam=1.5)
+    # print("\n")
+    # taylor_maccoll_from_shock(M1=1.5, shock_angle=np.radians(50), gam=1.5)
