262760334d9eb32e6ded45a64b2a41866bc6af61
Jared Magnusson <jmag722@gmail.com>
2025-07-05 16:08:28 -0500

    WIP #14: Tay-Mac with input shock angle passes tests. recursion no longer needed for taylor_maccoll_from_shock, integration stops automatically at input event, with adaptive stepping. small rtol/atol needed in RK45 integration to get more accurate answers, especially to converge for shock angles near the Mach angle.

--- minuteman/compressible/cone_flow.py
+++ minuteman/compressible/cone_flow.py
@@ -3,29 +3,119 @@ from scipy.integrate import solve_ivp
 import minuteman.compressible.oblique_shock as obs
 
 
-def compute_vprime(m: float, gam: float):
-    return (2 / ((gam-1) * m**2) + 1) ** -0.5
+def nondimensional_velocity_from_mach(M: float, gam: float):
+    """Compute the nondimensional velocity useful to nondimensionalizing the
+    Taylor Maccoll equations.
+
+    V'=V/Vmax from Anderson chapter 10, where Vmax is a a max theoretical velocity
+    if the flow expanded to 0 K (ho=h+V^2/2=Vmax^2/2). It is a function of Mach number only.
+
+    Args:
+        M (float): Mach number [-]
+        gam (float): ratio of specific heats [-]
+
+    Returns:
+        float: nondimensional velocity V'
+    """
+    return (2.0 / ((gam - 1.0) * M**2) + 1.0) ** -0.5
 
 
-def compute_vtheta_shock(vprime, shock_angle, deflection_angle):
-    return -np.sin(shock_angle - deflection_angle) * vprime
+def nondimensional_velocity_from_components(velocity_radial: float, velocity_azimuth: float):
+    """Compute the nondimensional velocity V' useful to nondimensionalizing the
+    Taylor Maccoll equations from its radial and azimuthal components,
+    V_r' and V_theta'.
+
+    Args:
+        velocity_radial (float): nondimensional radial velocity [-]
+        velocity_azimuth (float): nondimensional azimuth velocity [-]
+
+    Returns:
+        float: nondimensional velocity V' [-]
+    """
+    return np.linalg.norm((velocity_radial, velocity_azimuth), axis=0)
 
 
-def compute_vr_shock(vprime, shock_angle, deflection_angle):
-    return np.cos(shock_angle - deflection_angle) * vprime
+def mach_from_nondimensional_velocity(velocity: float, gam: float):
+    """Compute Mach number from nondimensional velocity
+
+    Args:
+        velocity (float): nondimensional velocity, V' [-]
+        gam (float): ratio of specific heats [-]
+
+    Returns:
+        float: Mach number
+    """
+    return (0.5 * (gam-1) * (velocity ** -2 - 1.0)) ** -0.5
 
 
-def fun(theta, y, gam):
-    vrprime, vthetaprime = y
-    x1 = vrprime
-    x2 = vthetaprime  # vtheta_prime = dvrprime/dtheta
-    dx1prime = x2
+def nondimensional_velocity_azimuth(velocity_nondim, shock_angle, deflection_angle):
+    """Compute the nondimensional azimuthal velocity behind a conic flow.
 
-    _term1 = (1-x1**2-x2**2)
-    dx2prime = (
-        ((gam-1)/2 * (2*x1*_term1 + x2/np.tan(theta)*_term1) - x1*x2**2) /
-        (x2**2 - (gam-1)/2 * _term1))
-    return dx1prime, dx2prime
+    This is V_theta' from Anderson chapter 10. The quantity is negative, as the
+    positive V_theta axis in the coordinate system is positive away from the
+    body.
+
+    Args:
+        velocity_nondim (Any): nondimensional velocity, V'
+        shock_angle (Any): shock angle [radians]
+        deflection_angle (Any): deflection angle [radians]
+
+    Returns:
+        Any: azimuth component of nondimensional velocity
+    """
+    return -np.sin(shock_angle - deflection_angle) * velocity_nondim
+
+
+def nondimensional_velocity_radial(velocity_nondim, shock_angle, deflection_angle):
+    """Compute the nondimensional radial velocity behind a conic flow.
+
+    This is V_r' from Anderson chapter 10. The quantity is positive in the
+    downstream direction
+
+    Args:
+        velocity_nondim (Any): nondimensional velocity, V'
+        shock_angle (Any): shock angle [radians]
+        deflection_angle (Any): deflection angle [radians]
+
+    Returns:
+        Any: radial component of nondimensional velocity
+    """
+    return np.cos(shock_angle - deflection_angle) * velocity_nondim
+
+
+def taylor_maccoll_odes(azimuth_angles: np.ndarray, nondim_velocity_components: tuple,
+                        gam: float):
+    """Return the Taylor-Maccoll 2nd-order ODE as a system of first-order ODE,
+    evaluated for a set of azimuth angles and nondimensional velocities.
+
+    Let y1 = V_r',
+
+    y2 = dV_r'/dtheta = V_theta'
+
+    Then,
+
+    dy1/dtheta = dV_r'/dtheta = y2
+
+    dy2/dtheta = d^2V_r'/dtheta^2
+
+    Args:
+        azimuth_angles (np.ndarray): azimuth angles at which to evaluate
+            ODE [radians]
+        nondim_velocity_components (tuple): radial, then azimuthal velocity components [-]
+        gam (float): ratio of specific heats [-]
+
+    Returns:
+        tuple: (dy1/dtheta, dy2/dtheta)
+    """
+    y1, y2 = nondim_velocity_components
+    dy1_dtheta = y2  # irrotational condition
+
+    gamma_term = 0.5 * (gam - 1)
+    # nondim. speed of sound squared, a^2
+    a2 = gamma_term * (1.0 - y1**2 - y2**2)
+    dy2_dtheta = (y1 * y2**2 - a2 * (2 * y1 + y2 / np.tan(azimuth_angles))
+                  ) / (a2 - y2**2)
+    return dy1_dtheta, dy2_dtheta
 
 
 def taylor_maccoll_from_cone(M1: float, cone_angle: float, gam: float = 1.4):
@@ -41,8 +131,6 @@ def taylor_maccoll_from_cone(M1: float, cone_angle: float, gam: float = 1.4):
     """
     initial_shock_guess = obs.shock_angle(
         M1=M1, theta=cone_angle, gam=gam)
-    initial_shock_guess = obs.mach_angle(M=M1)
-    initial_shock_guess = np.radians(34)
 
     print(f"Freestream Mach at gamma: {M1} at gam={gam}")
 
@@ -65,34 +153,49 @@ def taylor_maccoll_from_cone(M1: float, cone_angle: float, gam: float = 1.4):
 
 
 def _tay_mac_cone(shock_angle_guess: float, M1: float,
-                  cone_angle: float, gam: float,
+                  cone_angle: float, gam: float, x1=None, x2=None, fx1=None, fx2=None,
                   epsilon: float = 1e-15, ntheta: int = 100):
     mn1 = obs.mach1_normal(M1=M1, beta=shock_angle_guess)
     mn2 = obs.mach2_normal(Mn1=mn1, gam=gam)
     deflection_angle = obs.deflection_angle(
         M1=M1, beta=shock_angle_guess, gam=gam)
     m2 = obs.mach2(Mn2=mn2, beta=shock_angle_guess, theta=deflection_angle)
-    v_shock = compute_vprime(m=m2, gam=gam)
-    vtheta_shock = compute_vtheta_shock(vprime=v_shock, shock_angle=shock_angle_guess,
-                                        deflection_angle=deflection_angle)
-    vr_shock = compute_vr_shock(vprime=v_shock, shock_angle=shock_angle_guess,
-                                deflection_angle=deflection_angle)
+    v_shock = nondimensional_velocity_from_mach(M=m2, gam=gam)
+    vtheta_shock = nondimensional_velocity_azimuth(velocity_nondim=v_shock, shock_angle=shock_angle_guess,
+                                                   deflection_angle=deflection_angle)
+    vr_shock = nondimensional_velocity_radial(velocity_nondim=v_shock, shock_angle=shock_angle_guess,
+                                              deflection_angle=deflection_angle)
 
     theta_arr = np.linspace(shock_angle_guess, cone_angle, ntheta)
-    solution = solve_ivp(fun=fun, t_span=(
+    solution = solve_ivp(fun=taylor_maccoll_odes, t_span=(
         shock_angle_guess, cone_angle), y0=(vr_shock, vtheta_shock), args=(gam,), t_eval=theta_arr)
 
-    sol_vr, sol_vtheta = solution.y
-    cone_angle_new = (cone_angle - sol_vtheta[-1] /
-                      fun(theta=theta_arr[-1], y=(sol_vr[-1], sol_vtheta[-1]), gam=gam)[-1])
-    # we know the actual cone angle (it's cone_angle), so adjust the new shock
-    # angle by the amount needed to get vtheta(theta_c)=0
-    shock_angle_new = shock_angle_guess - (cone_angle_new - cone_angle)
-    if np.abs(cone_angle_new - cone_angle) < epsilon:
+    v_r, v_theta = solution.y
+
+    if fx1 is None or fx2 is None:
+        cone_angle_new = (cone_angle - v_theta[-1] /
+                          taylor_maccoll_odes(azimuth_angles=theta_arr[-1],
+                                              nondim_velocity_components=(
+                                              v_r[-1], v_theta[-1]),
+                                              gam=gam)[-1])
+        # we know the actual cone angle (it's cone_angle), so adjust the new shock
+        # angle by the amount needed to get vtheta(theta_c)=0
+        shock_angle_new = shock_angle_guess - (cone_angle_new - cone_angle)
+        shock_angle_new = np.maximum(shock_angle_new, obs.mach_angle(M=M1))
+    else:
+        shock_angle_new = x1 - fx1 * (x1 - x2) / (fx1 - fx2)
+        shock_angle_new = np.maximum(shock_angle_new, obs.mach_angle(M=M1))
+    print(f"Vtheta at surf: {v_theta[-1]}")
+    print(f"Cone angle: {np.degrees(shock_angle_new)}")
+    fx2 = fx1
+    fx1 = v_theta[-1]
+    x2 = x1
+    x1 = shock_angle_guess
+    if np.abs(v_theta[-1]) < epsilon:
         return solution
     else:
         return _tay_mac_cone(shock_angle_guess=shock_angle_new, M1=M1,
-                             cone_angle=cone_angle, gam=gam)
+                             cone_angle=cone_angle, gam=gam, x1=x1, x2=x2, fx1=fx1, fx2=fx2)
 
 
 def taylor_maccoll_from_shock(M1: float, shock_angle: float, gam: float = 1.4):
@@ -111,86 +214,25 @@ def taylor_maccoll_from_shock(M1: float, shock_angle: float, gam: float = 1.4):
     mn2 = obs.mach2_normal(Mn1=mn1, gam=gam)
     deflection_angle = obs.deflection_angle(M1=M1, beta=shock_angle, gam=gam)
     m2 = obs.mach2(Mn2=mn2, beta=shock_angle, theta=deflection_angle)
-    v_shock = compute_vprime(m=m2, gam=gam)
-    vtheta_shock = compute_vtheta_shock(vprime=v_shock, shock_angle=shock_angle,
-                                        deflection_angle=deflection_angle)
-    vr_shock = compute_vr_shock(vprime=v_shock, shock_angle=shock_angle,
-                                deflection_angle=deflection_angle)
-
-    print(f"Freestream Mach at gamma: {M1} at gam={gam}")
-    print(f"Shock Angle: {np.degrees(shock_angle)} deg")
-    print(f"Deflection Angle: {np.degrees(deflection_angle)} deg")
-
-    solution = _tay_mac(
-        cone_angle_guess=deflection_angle*2,
-        vr_shock=vr_shock,
-        vtheta_shock=vtheta_shock, shock_angle=shock_angle,
-        gam=gam)
-    vr, vtheta = solution.y
-    theta = solution.t
-    sol_vprime = (vr**2 + vtheta**2)**0.5
-    sol_mach = ((sol_vprime**-2 - 1) * (gam-1)/2) ** -0.5
-
-    print(f"Mach at surface: {sol_mach[-1]}")
-    print(f"Cone angle = {np.degrees(theta[-1])} deg")
-    print(f"Vtheta at cone = {np.degrees(vtheta[-1])}")
-
-
-def _tay_mac(cone_angle_guess: float, vr_shock: float, vtheta_shock: float,
-             shock_angle: float, gam: float,
-             epsilon: float = 1e-15, ntheta: int = 100):
-    """Recursive formula to solve the Taylor-Maccoll problem
-
-    Args:
-        cone_angle_guess (float): estimate for the cone angle [radians]
-        vr_shock (float): nondimensional velocity in the radial direction
-            immediately behind the shock
-        vtheta_shock (float): nondimensional velocity in the angular direction
-            immediately behind the shock
-        shock_angle (float): shock angle [radians]
-        gam (float): ratio of specific heats
-        epsilon (float, optional): minimum required difference in cone angle.
-            Defaults to 1e-15.
-        ntheta (int, optional): number of angular points in numerical integration
-            from the shock to the surface. Defaults to 100.
-
-    Raises:
-        ValueError: vtheta_shock is non-negative
-
-    Returns:
-        _type_: solution of numerical integral
-    """
+    v_shock = nondimensional_velocity_from_mach(M=m2, gam=gam)
+    vr_shock = nondimensional_velocity_radial(velocity_nondim=v_shock, shock_angle=shock_angle,
+                                              deflection_angle=deflection_angle)
+    vtheta_shock = nondimensional_velocity_azimuth(velocity_nondim=v_shock, shock_angle=shock_angle,
+                                                   deflection_angle=deflection_angle)
     if vtheta_shock >= 0.0:
         raise ValueError("Angular velocity at the shock must be negative!")
-    theta_arr = np.linspace(shock_angle, cone_angle_guess, ntheta)
-    solution = solve_ivp(fun=fun, t_span=(
-        shock_angle, cone_angle_guess), y0=(vr_shock, vtheta_shock), args=(gam,), t_eval=theta_arr)
 
-    sol_vr, sol_vtheta = solution.y
-    vrc = sol_vr[-1]
-    vthetac = sol_vtheta[-1]
-    cone_angle_new = (cone_angle_guess - vthetac /
-                      fun(theta=theta_arr[-1], y=(vrc, vthetac), gam=gam)[-1])
-    if np.abs(cone_angle_new - cone_angle_guess) < epsilon:
-        return solution
-    else:
-        return _tay_mac(cone_angle_guess=cone_angle_new, vr_shock=vr_shock,
-                        vtheta_shock=vtheta_shock, shock_angle=shock_angle, gam=gam)
+    def vtheta_zero_event(t, y, gam: float):
+        return y[1]
+    vtheta_zero_event.terminal = True
+    vtheta_zero_event.direction = 1.0
+    solution = solve_ivp(fun=taylor_maccoll_odes, t_span=(shock_angle, 0.0),
+                         y0=(vr_shock, vtheta_shock), args=(gam,),
+                         events=vtheta_zero_event, atol=1e-18, rtol=1e-13)
 
+    if not solution.success:
+        raise AssertionError("RK integration step failed, check inputs.")
 
-if __name__ == "__main__":
-    # taylor_maccoll_from_shock(M1=3.0, shock_angle=np.radians(25), gam=1.4)
-    # print("\n")
-    # taylor_maccoll_from_shock(M1=7.0, shock_angle=np.radians(80), gam=1.4)
-    # print("\n")
-    # taylor_maccoll_from_shock(M1=4.0, shock_angle=np.radians(33), gam=1.5)
-    # print("\n")
-    # taylor_maccoll_from_shock(M1=1.5, shock_angle=np.radians(50), gam=1.5)
-
-    taylor_maccoll_from_cone(M1=11.0, cone_angle=np.radians(25), gam=1.6)
-    print("\n")
-    # taylor_maccoll_from_shock(M1=7.0, shock_angle=np.radians(80), gam=1.4)
-    # print("\n")
-    # taylor_maccoll_from_shock(M1=4.0, shock_angle=np.radians(33), gam=1.5)
-    # print("\n")
-    # taylor_maccoll_from_shock(M1=1.5, shock_angle=np.radians(50), gam=1.5)
+    vr, vtheta = solution.y
+    theta = solution.t
+    return (theta, vr, vtheta)
--- tests/minuteman/compressible/test_cone_flow.py
+++ tests/minuteman/compressible/test_cone_flow.py
@@ -1,1 +1,40 @@
+import numpy as np
+import pytest
 import minuteman.compressible.cone_flow as cf
+
+
+@pytest.mark.parametrize("M1, shock_angle, gam, theta_c, mach_c, rtol_theta, rtol_mach", [
+    (1.5, 42.0, 1.4, 6.67715193, 1.43307833, 1e-3, None),  # VT
+    (1.5, 42.0, 1.4, 6.680016719152055, 1.4330783951611243, 1e-5, None),  # SAEMiller
+    (5, 20.0, 1.3, 15.269259373069906, 4.002902231709606, None, None),  # VT
+    (20.0, np.degrees(0.06734690), 1.4, 2.5, 17.637850, 1e-5, 1e-5),  # Sims
+    (15, np.degrees(0.58391003), 1.4, 30.0, 3.3037471, None, None),  # Sims
+    (1.75, np.degrees(0.87135164), 1.4, 27.5, 1.1495789, 1e-5, None),  # Sims
+    (7.0, np.degrees(0.23632593), 1.4, 10, 5.6318010, None, None),  # Sims
+    (2.0, 31, 1.5, 9.274242549862922, 1.83629035381199,
+     1e-5, None),  # SAEMiller
+])
+def test_taylor_maccoll_from_shock(M1, shock_angle, gam, theta_c, mach_c,
+                                   rtol_theta, rtol_mach):
+    """Check Taylor-Maccoll relations with input shock angle.
+
+    Unit tests taken from the following sources:
+
+    Sims, Joseph L. "Tables for supersonic flow around right circular cones at
+        zero angle of attack." (1964).
+    Fluid Dynamics Flow Calculators - Prof. S. A. E. Miller:
+        https://saemiller.com/flow/SAEMiller_Comp_Calc.html
+    Compressible Aerodynamics Calculator v3.2:
+        https://devenport.aoe.vt.edu/aoe3114/calc.html
+
+    I get better agreement with SAEMiller calculator than VT calculator.
+    """
+    rtol_theta = rtol_theta if rtol_theta is not None else 5e-6
+    rtol_mach = rtol_mach if rtol_mach is not None else 5e-6
+    theta, vr, vtheta = cf.taylor_maccoll_from_shock(
+        M1=M1, shock_angle=np.radians(shock_angle), gam=gam)
+    theta = np.degrees(theta)
+    v = cf.nondimensional_velocity_from_components(vr, vtheta)
+    mach = cf.mach_from_nondimensional_velocity(v, gam=gam)
+    assert theta[-1] == pytest.approx(theta_c, rel=rtol_theta)
+    assert mach[-1] == pytest.approx(mach_c, rel=rtol_mach)
